0144815bd291a4be8cf614a51359700a3262fd2e
Sheldon <mcgr0235@algonquinlive.com>
2017-04-13 11:55:23 -0400

    commented missed files

--- src/main/java/io/github/vanm0012/symphonymanager/util/Date.java
+++ src/main/java/io/github/vanm0012/symphonymanager/util/Date.java
@@ -1,21 +1,39 @@
 package io.github.vanm0012.symphonymanager.util;
 
+/**
+ * This class hold and manipulates date data
+ */
 public class Date
 {
     private String date;
 
+    /**
+     * default constructor
+     */
     public Date() {}
 
+    /**
+     * Constructor sets the data
+     * @param date
+     */
     public Date(String date)
     {
         this.date = date;
     }
 
+    /**
+     * Sets a new data from given String
+     * @param date new date
+     */
     public void setDate(String date)
     {
         this.date = date;
     }
 
+    /**
+     * Returns set data
+     * @return date
+     */
     public String getDate()
     {
         return date;
--- src/main/java/io/github/vanm0012/symphonymanager/util/DateTime.java
+++ src/main/java/io/github/vanm0012/symphonymanager/util/DateTime.java
@@ -1,22 +1,41 @@
 package io.github.vanm0012.symphonymanager.util;
 
+/**
+ * This class holds the time related to a date
+ */
 public class DateTime extends Date
 {
     private String time;
 
+    /**
+     * Default constructor
+     */
     public DateTime() {}
 
+    /**
+     * Constructor sets the time and date
+     * @param date the date of an event
+     * @param time the time of an event
+     */
     public DateTime(String date, String time)
     {
         super(date);
         this.time = time;
     }
 
+    /**
+     * Sets a new time from a given String
+     * @param time
+     */
     public void setTime(String time)
     {
         this.time = time;
     }
 
+    /**
+     * Returns the set time
+     * @return Time
+     */
     public String getTime()
     {
         return time;
