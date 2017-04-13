24f4b1a2850b541868cf680f60eeb4fb1a8ec002
Tryton Van Meer <trytonvanmeer@gmail.com>
2017-04-13 12:49:05 -0400

    Added ConcertSeason class with basics finished

---
+++ src/main/java/io/github/vanm0012/symphonymanager/domain/ConcertSeason.java
@@ -0,0 +1,59 @@
+package io.github.vanm0012.symphonymanager.domain;
+
+import io.github.vanm0012.symphonymanager.util.Date;
+
+import java.util.ArrayList;
+
+public class ConcertSeason
+{
+    private ArrayList<Concert> concerts;
+    private Date openDate;
+    private int duration;
+
+    public ConcertSeason()
+    {
+        concerts = new ArrayList<>();
+    }
+
+    public ConcertSeason(ArrayList<Concert> concerts, Date openDate, int duration)
+    {
+        this.concerts = concerts;
+        this.openDate = openDate;
+        this.duration = duration;
+    }
+
+    public void setConcerts(ArrayList<Concert> concerts)
+    {
+        this.concerts = concerts;
+    }
+
+    public void setOpenDate(Date openDate)
+    {
+        this.openDate = openDate;
+    }
+
+    public void setDuration(int duration)
+    {
+        this.duration = duration;
+    }
+
+    public ArrayList<Concert> getConcerts()
+    {
+        return concerts;
+    }
+
+    public Date getOpenDate()
+    {
+        return openDate;
+    }
+
+    public int getDuration()
+    {
+        return duration;
+    }
+
+    public void addConcert(Concert concert)
+    {
+        this.concerts.add(concert);
+    }
+}
