528c4d63e43ba05b03595bb91240d5e72a081b0b
Sheldon <mcgr0235@algonquinlive.com>
2017-04-13 13:36:04 -0400

    commented concert and concertseason

--- src/main/java/io/github/vanm0012/symphonymanager/domain/Concert.java
+++ src/main/java/io/github/vanm0012/symphonymanager/domain/Concert.java
@@ -14,11 +14,21 @@ public class Concert
     private String id;
     private Conductor conductor;
 
+    /**
+     * Default constructor initializes the compositions arraylist
+     */
     public Concert()
     {
         this.compositions = new ArrayList<>();
     }
 
+    /**
+     * Constructor sets all data members
+     * @param compositions The arraylist of compositions
+     * @param startDate The start of the concert
+     * @param id The ID number of the concert
+     * @param conductor The name of the conductor
+     */
     public Concert(ArrayList<Composition> compositions, DateTime startDate, String id, Conductor conductor)
     {
         this.compositions = compositions;
@@ -27,46 +37,82 @@ public class Concert
         this.conductor = conductor;
     }
 
+    /**
+     * Sets  a new arraylist of compositions
+     * @param compositions new compositions arraylist
+     */
     public void setCompositions(ArrayList<Composition> compositions)
     {
         this.compositions = compositions;
     }
 
+    /**
+     * Sets a new start date for the concert from a given DateTime
+     * @param startDate Start date
+     */
     public void setStartDate(DateTime startDate)
     {
         this.startDate = startDate;
     }
 
+    /**
+     * Sets a new ID for the concert from a given String
+     * @param id new ID
+     */
     public void setId(String id)
     {
         this.id = id;
     }
 
+    /**
+     * Sets a new conductor for a concert from a given Conductor object
+     * @param conductor new conductor
+     */
     public void setConductor(Conductor conductor)
     {
         this.conductor = conductor;
     }
 
+    /**
+     * Returns the current composition list
+     * @return Arraylist of compositions
+     */
     public ArrayList<Composition> getCompositions()
     {
         return compositions;
     }
 
+    /**
+     * Returns the current start date of the concert
+     * @return Start date
+     */
     public DateTime getStartDate()
     {
         return startDate;
     }
 
+    /**
+     * Returns the current ID for a concert
+     * @return ID of concert
+     */
     public String getId()
     {
         return id;
     }
 
+    /**
+     * Returns current Conductor object
+     * @return Conductor of concert
+     */
     public Conductor getConductor()
     {
         return conductor;
     }
 
+    /**
+     * Adds a composition to the arraylist from a given Composition object
+     * @param composition new composition to be added
+     */
     public void addComposition(Composition composition)
     {
         this.compositions.add(composition);
--- src/main/java/io/github/vanm0012/symphonymanager/domain/ConcertSeason.java
+++ src/main/java/io/github/vanm0012/symphonymanager/domain/ConcertSeason.java
@@ -5,17 +5,29 @@ import io.github.vanm0012.symphonymanager.util.Date;
 
 import java.util.ArrayList;
 
+/**
+ * Holds and manipulates data for a concert season
+ */
 public class ConcertSeason
 {
     private ArrayList<Concert> concerts;
     private Date openDate;
     private int duration;
 
+    /**
+     * Default constructor
+     */
     public ConcertSeason()
     {
         concerts = new ArrayList<>();
     }
 
+    /**
+     * Constructor adds all data members from given variables
+     * @param concerts Arraylist of the concerts in a season
+     * @param openDate The start date of a season
+     * @param duration The length of a concert season
+     */
     public ConcertSeason(ArrayList<Concert> concerts, Date openDate, int duration)
     {
         this.concerts = concerts;
@@ -23,41 +35,75 @@ public class ConcertSeason
         this.duration = duration;
     }
 
+    /**
+     * Sets a new arraylist of the concerts from a given arraylist
+     * @param concerts new arraylist of concerts
+     */
     public void setConcerts(ArrayList<Concert> concerts)
     {
         this.concerts = concerts;
     }
 
+    /**
+     * Sets a new open date for the season from a given Date
+     * @param openDate new open date
+     */
     public void setOpenDate(Date openDate)
     {
         this.openDate = openDate;
     }
 
+    /**
+     * Sets a new length of a season from a given integer
+     * @param duration new duration
+     */
     public void setDuration(int duration)
     {
         this.duration = duration;
     }
 
+    /**
+     * Returns the current list of concerts in a season
+     * @return current arraylist of concerts
+     */
     public ArrayList<Concert> getConcerts()
     {
         return concerts;
     }
 
+    /**
+     * Returns the Current open date for the season
+     * @return Current open date for the season
+     */
     public Date getOpenDate()
     {
         return openDate;
     }
 
+    /**
+     * Returns the current length of the season
+     * @return Current duration of season
+     */
     public int getDuration()
     {
         return duration;
     }
 
+    /**
+     * Adds a concert to the list from given concert object
+     * @param concert New concert to be added
+     */
     public void addConcert(Concert concert)
     {
         this.concerts.add(concert);
     }
 
+    /**
+     * Returns A concert at index given
+     * @param index index of concert requested
+     * @return The concert requested
+     * @throws ConcertSeasonIndexOutOfBoundsException Exception if the index it out of the list's bounds
+     */
     public Concert getConcert(int index) throws ConcertSeasonIndexOutOfBoundsException
     {
         try
