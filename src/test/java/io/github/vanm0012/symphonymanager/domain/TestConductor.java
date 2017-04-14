786d48dcf1603eb6a3aaac058a5594bf163c18a9
Tryton Van Meer <trytonvanmeer@gmail.com>
2017-04-14 19:04:44 -0400

    Finished test case for conductor

--- src/test/java/io/github/vanm0012/symphonymanager/domain/TestConductor.java
+++ src/test/java/io/github/vanm0012/symphonymanager/domain/TestConductor.java
@@ -6,14 +6,21 @@ import static org.junit.Assert.*;
 
 public class TestConductor
 {
+    private int testId = 1234;
+
     @Test
     public void setId()
     {
+        Conductor conductor = new Conductor();
+        conductor.setId(testId);
+        assertEquals(conductor.getId(), testId);
     }
 
     @Test
     public void getId()
     {
+        Conductor conductor = new Conductor("Joe Dong", "12 Pear Dr.", 1234567, testId);
+        assertEquals(conductor.getId(), testId);
     }
 
 }
