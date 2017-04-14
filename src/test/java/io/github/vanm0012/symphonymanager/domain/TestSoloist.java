a878e54e5f19dff57f91976d4e340f7841095146
Sheldon <mcgr0235@algonquinlive.com>
2017-04-14 19:17:53 -0400

    wrote test cases for soloist and symphony employee

--- src/test/java/io/github/vanm0012/symphonymanager/domain/TestSoloist.java
+++ src/test/java/io/github/vanm0012/symphonymanager/domain/TestSoloist.java
@@ -6,14 +6,20 @@ import static org.junit.Assert.*;
 
 public class TestSoloist
 {
+    String testID = "123abc";
+    Soloist solo = new Soloist();
     @Test
     public void setId()
     {
+        solo.setId(testID);
+        assertEquals(solo.getId(),testID);
+
     }
 
     @Test
     public void getId()
     {
+        assertEquals(solo.getId(),testID);
     }
 
 }
--- src/test/java/io/github/vanm0012/symphonymanager/domain/TestSymphonyEmployee.java
+++ src/test/java/io/github/vanm0012/symphonymanager/domain/TestSymphonyEmployee.java
@@ -5,35 +5,48 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestSymphonyEmployee
+
 {
+    String testName = "Reg", testAddress = "123 Street rd.";
+    int phone = 5555555;
+    SymphonyEmployee se = new SymphonyEmployee();
     @Test
     public void setName()
     {
+        se.setName(testName);
+        assertEquals(se.getName(),testName);
     }
 
     @Test
     public void setAddress()
     {
+        se.setAddress(testAddress);
+        assertEquals(se.getAddress(),testAddress);
     }
 
     @Test
     public void setPhone()
     {
+        se.setPhone(phone);
+        assertEquals(se.getPhone(),phone);
     }
 
     @Test
     public void getName()
     {
+        assertEquals(se.getName(),testName);
     }
 
     @Test
     public void getAddress()
     {
+        assertEquals(se.getAddress(),testAddress);
     }
 
     @Test
     public void getPhone()
     {
+        assertEquals(se.getPhone(),phone);
     }
 
 }
